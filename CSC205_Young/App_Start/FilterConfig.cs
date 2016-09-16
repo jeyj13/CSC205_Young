e57f05137b210a7b7768662a5b761d14d28dd191
unknown <jeyj13@gmail.com>
2016-09-15 20:56:46 -0400

    9/15/2016 initial commit

---
+++ CSC205_Young/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CSC205_Young
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
